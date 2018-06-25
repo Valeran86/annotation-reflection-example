cde10d04e2df3ef612aee91eb53b17269560642a
Valeran86 <uq8cysjd>
2018-06-25 23:15:03 +0500

    fix task

--- src/main/java/ru/sbt/examples/reflection/ReflectionExample.java
+++ src/main/java/ru/sbt/examples/reflection/ReflectionExample.java
@@ -6,7 +6,7 @@ import org.apache.commons.lang.builder.ReflectionToStringBuilder;
  * I. Рефлексия
  * 1.Вывести на консоль все методы класса Double, включая все родительские методы  (включая приватные)
  * <p>
- * 2. Вывести все геттеры класса Number
+ * 2. Вывести все геттеры класса Class
  * <p>
  * 3. Проверить, что все String константы имеют значение = их имени
  * public static final String MONDAY = "MONDAY";
