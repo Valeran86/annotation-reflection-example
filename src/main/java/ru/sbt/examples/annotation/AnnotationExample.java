9ce09957189c8a657684e577ead42283e1e0e245
Валерий <vtaranukha@gmail.com>
2018-06-29 23:43:18 +0500

    1

--- src/main/java/ru/sbt/examples/annotation/AnnotationExample.java
+++ src/main/java/ru/sbt/examples/annotation/AnnotationExample.java
@@ -1,5 +1,8 @@
 package ru.sbt.examples.annotation;
 
+import javax.persistence.*;
+import java.lang.annotation.*;
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,7 +20,7 @@ import java.util.List;
  */
 public class AnnotationExample {
 
-    public static void main( String... args ) {
+    public static void main( String... args ) throws IllegalAccessException {
         List<Object> ormExampleList = Arrays.asList(
                 ORMExample.builder()
                         .name( "вот такое имя хочу сохранить в БД" )
@@ -28,7 +31,7 @@ public class AnnotationExample {
                         .active( Boolean.TRUE )
                         .build()
                 , ORMExample2.builder()
-                        .id( 1 )
+                        //.id( 1 )
                         .service( "очень-очень длинная строка очень-очень длинная строка очень-очень длинная строка очень-очень длинная строка очень-очень длинная строка очень-очень длинная строка очень-очень длинная строка " )
                         .price( 123.45 )
                         .build()
@@ -48,12 +51,58 @@ public class AnnotationExample {
         }
     }
 
-    private static void checkColumnLimitation( Object object ) {
-        // добавить проверки
-        System.out.println("sdfsdf");
+    private static void checkColumnLimitation( Object object ) throws IllegalAccessException {
+        Class<?> clazz = object.getClass();
+        boolean acc;
+        Column column;
+        int length;
+        String name;
+        boolean nullable;
+        int precision;
+        for(Field field : clazz.getDeclaredFields()){
+            if(!field.isAnnotationPresent(Column.class)) continue;
+            column = field.getAnnotation(Column.class);
+            length = column.length();
+            nullable = column.nullable();
+            precision = column.precision();
+
+            acc = field.isAccessible();
+            field.setAccessible(true);
+            Object value = field.get(object);
+            if(value==null && nullable == false) System.out.println("В объекте " + object.toString() +
+                " поле " + field.getName() + " нарушает аннотацию Column nullable = false");
+            if(Number.class.isAssignableFrom(field.getType())){
+                if(!((Double) value).toString().matches("\\d*\\.\\d{0," + precision +"}")) {
+                    System.out.println("В объекте " + object.toString() +
+                            " поле " + field.getName() + " нарушает аннотацию Column precision = " + precision);
+                }
+            }
+            if(field.getType() == String.class){
+                if(value.toString().length()>length) {
+                    System.out.println("В объекте " + object.toString() +
+                            " поле " + field.getName() + " нарушает аннотацию Column length = " + length);
+                }
+            }
+
+            field.setAccessible(acc);
+        }
     }
 
-    private static void checkPrimaryKey( Object object ) {
-        // добавить проверки
+    private static void checkPrimaryKey( Object object ) throws IllegalAccessException {
+        Class<?> clazz = object.getClass();
+        boolean acc;
+        for(Field field : clazz.getDeclaredFields()){
+            //System.out.println("Поле: " + field.getName());
+            if(!field.isAnnotationPresent(Id.class)) continue;
+            //System.out.println("Это поле ID");
+            if(field.isAnnotationPresent(GeneratedValue.class)) continue;
+            //System.out.println("Это геренрируемое поле");
+            acc = field.isAccessible();
+            field.setAccessible(true);
+            if (field.get(object)==null){
+                System.out.println("Поле ID пустое для объекта " + object.toString());
+            }
+            field.setAccessible(acc);
+        }
     }
 }
--- src/main/java/ru/sbt/examples/reflection/ReflectionExample.java
+++ src/main/java/ru/sbt/examples/reflection/ReflectionExample.java
@@ -2,6 +2,12 @@ package ru.sbt.examples.reflection;
 
 import org.apache.commons.lang.builder.ReflectionToStringBuilder;
 
+import java.lang.reflect.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.*;
+
 /**
  * I. Рефлексия
  * 1.Вывести на консоль все методы класса Double, включая все родительские методы  (включая приватные)
@@ -18,9 +24,75 @@ public class ReflectionExample {
     public static final String FAIL_CONSTANT = "bad_constant_name";
 
     public static void main( String... args ) {
-        Number n = Double.valueOf( 1d );
+//        Number n = Double.valueOf( 1d );
+//
+//        System.out.println( n );
+//        System.out.println( ReflectionToStringBuilder.toString( n ) );
+        allMethodsIncludePrivate(Double.class);
+        allGettersOfClass(Class.class);
+        checkAllConstants(ReflectionExample.class);
+    }
+    private static void allMethodsIncludePrivate(Class clazz){
+        do {
+            Method allM[] = clazz.getDeclaredMethods();
+            for (int i = 0; i < allM.length; i++) {
+                System.out.println(allM[i]);
+            }
+            clazz = clazz.getSuperclass();
+        }while(clazz != null);
+    }
+    private class My{
+        private Double p1;
 
-        System.out.println( n );
-        System.out.println( ReflectionToStringBuilder.toString( n ) );
+        public Double getP1() {
+            return p1;
+        }
+
+        public void setP1(Double p1) {
+            this.p1 = p1;
+        }
+        private void met(){
+        }
+    }
+    private static void allGettersOfClass(Class clazz){
+        Field allF[] = clazz.getDeclaredFields();
+        Method allM[] = clazz.getDeclaredMethods();
+        String str;
+        Pattern p1;
+        Matcher m1;
+
+        for (int i = 0; i < allF.length; i++) {
+            p1 = Pattern.compile("(\\S+\\s\\S+)" + allF[i].getName());
+            m1 = p1.matcher(allF[i].toString());
+            if(m1.find()) {
+                str = ".*\\s" + m1.group(1) + "get" + allF[i].getName().substring(0,1).toUpperCase()+ allF[i].getName().substring(1) + "\\(\\)";
+                str= str.replace("$", "\\$");
+                str= str.replace("[", "\\[");
+                str= str.replace("]", "\\]");
+                for (int j = 0; j < allM.length; j++) {
+                    if (allM[j].toString().matches(str)) {
+                        System.out.println(allM[j]);
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+    private static <T> void checkAllConstants(Class clazz){
+        Field allF[] = clazz.getDeclaredFields();
+        boolean acc;
+        for (int i=0;i<allF.length;i++){
+            if(allF[i].getType() != String.class) continue;
+            if(!allF[i].toString().matches(".*\\sfinal\\s.+")) continue;
+            acc = allF[i].isAccessible();
+            allF[i].setAccessible(true);
+            // дальше непонятно как получить значение этой константы, чтобы сравнить с его именем
+//            String value=allF[i].get(???);
+//            if(value != allF[i].getName()) System.out.println("Константа " + allF[i].getName()
+//                    + " имеет значение " + value + " не совпадающее с именем!");
+            allF[i].setAccessible(acc);
+        }
+
     }
 }
