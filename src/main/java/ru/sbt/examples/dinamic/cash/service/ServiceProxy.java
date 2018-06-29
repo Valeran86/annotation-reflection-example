0147a28f26569a8d5bdaf05833a5cd36354e93fe
Sherbakov_A_AS <Smersh.Root@gmail.com>
2018-06-30 01:18:44 +0500

    Revert "Рефакторинг Кэширующего прокси. Переделал на работу через специализированные объекты. Так гораздо понятнее с точки зрения разработки. И работать с файлом кэша будет проще. Измененные для этой цели классы ProxyObject, ProxyObjectManager, ServiceProxy."
    This reverts commit 2c939f0

--- src/main/java/ru/sbt/examples/dinamic/cash/service/ServiceProxy.java
+++ src/main/java/ru/sbt/examples/dinamic/cash/service/ServiceProxy.java
@@ -1,15 +1,15 @@
 package ru.sbt.examples.dinamic.cash.service;
 
-import ru.sbt.examples.dinamic.cash.proxy.ProxyObject;
-import ru.sbt.examples.dinamic.cash.proxy.ProxyObjectManager;
 import ru.sbt.examples.dinamic.cash.proxy.annotation.CacheDynamicProxy;
 import ru.sbt.examples.dinamic.cash.proxy.constants.Constants;
 
 import java.lang.reflect.Method;
 import java.util.*;
+import java.io.*;
 
 public class ServiceProxy implements java.lang.reflect.InvocationHandler {
     private Object obj;
+    private HashMap<List<Object>,Object> cacheHashyMap=new HashMap<>();
     private boolean isCasheNewList=false;
     private int countList=-1;
 
@@ -38,53 +38,54 @@ public class ServiceProxy implements java.lang.reflect.InvocationHandler {
 
     private Object casheFromMemory(CacheDynamicProxy annotationCacheDynamicProxy, Method method, Object[] args)
             throws Throwable{
-        ProxyObject proxyObject=getCasheProxyObject(annotationCacheDynamicProxy,method,args);
-        Object returnValue=proxyObject.getReturnValue();
-        if (returnValue!=null){
+        List<Object> paramsCache=getParamsCache(annotationCacheDynamicProxy,method,args);
+
+        if(cacheHashyMap.containsKey(paramsCache)){
             System.out.println("INFO: Значение взято из кэша");
-        }else {
+            return cacheHashyMap.get(paramsCache);
+        }else{
             System.out.println("INFO: Значение расчитано");
             Object noCacheResult=method.invoke(obj, args);
-            returnValue=noCacheResult;
+            Object inCacheResult=noCacheResult;
             if(isCasheNewList){
-                noCacheResult=(((List)noCacheResult).subList(0, this.countList));
+                inCacheResult=(((List)noCacheResult).subList(0, this.countList));
                 isCasheNewList=false;
                 this.countList=0;
             }
+            cacheHashyMap.put(paramsCache,(inCacheResult));
 
-            ProxyObjectManager.addReturnValueInProxyObject(proxyObject,noCacheResult);
-
+            return noCacheResult;
         }
-        return returnValue;
     }
 
-
-    private ProxyObject getCasheProxyObject(CacheDynamicProxy annotationCacheDynamicProxy, Method method, Object[] args){
-        HashMap<Class,Object> listParamValue=new HashMap<>();
-        HashMap<String,Object> hashMapAttrAnnotation=new HashMap<>();
-
+    private List<Object> getParamsCache(CacheDynamicProxy annotationCacheDynamicProxy, Method method, Object[] args){
+        List<Object> paramsCache=new ArrayList<>();
+        paramsCache.add(method);
         List identityBy=Arrays.asList(annotationCacheDynamicProxy.identityBy());
+        HashMap<Class,Object> listParamValue=new HashMap<>();
         if(args!=null){
             for(Object arg:args){
                 if(identityBy.size()==0 ||identityBy.contains(arg.getClass())){
                     listParamValue.put(arg.getClass(),arg);
                 }
             }
+            paramsCache.add(listParamValue);
         }
+
         int countList=annotationCacheDynamicProxy.countList();
         if(countList>=0 && method.getReturnType().equals(List.class)){
             this.isCasheNewList=true;
-            hashMapAttrAnnotation.put("countList",countList);
+            paramsCache.add("List size:"+countList);
             this.countList=countList;
         }
 
-        return  ProxyObjectManager.proxyObjectContains(method,listParamValue,hashMapAttrAnnotation);
 
+        return paramsCache;
     }
 
     private Object casheFromFile(CacheDynamicProxy annotationCacheDynamicProxy, Method method, Object[] args)
             throws Throwable{
-
+        List<Object> paramsCache=getParamsCache(annotationCacheDynamicProxy,method,args);
 //TODO предстоит сделать работу с файлами
         if(/*Есть в файле*/1!=1){
             System.out.println("INFO: Значение взято из файла");
